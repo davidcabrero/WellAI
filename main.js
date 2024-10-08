2d605f84da81549209eb747b46a763acbbe97d18
davidcabrerojimenez <davidcabrerojimenez@gmail.com>
2024-10-08 20:46:22 +0000

    Creado interfaz y modelo de IA

---
+++ main.js
@@ -0,0 +1,57 @@
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+import Base64 from 'base64-js';
+import MarkdownIt from 'markdown-it';
+import './style.css';
+
+
+let API_KEY = 'AIzaSyDdFJjfcanrCIfBcltNksz1CJXl8Z27US8';
+
+let form = document.querySelector('form');
+let promptInput = document.querySelector('input[name="prompt"]');
+let output = document.querySelector('.output');
+
+form.onsubmit = async (ev) => {
+  ev.preventDefault();
+  output.textContent = 'Cargando...';
+
+  try {
+    // Recibe los datos introducidos por el usuario
+    let userText = document.getElementById("user-text").value;
+
+    let prompt = `${userText}`;
+
+    // Assemble the prompt by combining the text with the chosen image
+    let contents = [
+      {
+        role: 'user',
+        parts: [
+          { text: prompt }
+        ]
+      }
+    ];
+
+    // Call the multimodal model, and get a stream of results
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash", // or gemini-1.5-pro
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        },
+      ],
+    });
+
+    const result = await model.generateContentStream({ contents });
+
+    // Read from the stream and interpret the output as markdown
+    let buffer = [];
+    let md = new MarkdownIt();
+    for await (let response of result.stream) {
+      buffer.push(response.text());
+      output.innerHTML = md.render(buffer.join(''));
+    }
+  } catch (e) {
+    output.innerHTML += '<hr>' + e;
+  }
+};
